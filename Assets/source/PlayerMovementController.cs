deace367589e570716f6a7e1553b99ba779a0b50
lowkey <mephistopheles@onlinehome.de>
2018-01-26 21:27:03 +0100

    added basic player controller and standard assets

---
+++ Assets/source/PlayerMovementController.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (PlayerMovement))]
+public class PlayerMovementController : MonoBehaviour
+{
+	private PlayerMovement m_Character;
+    private bool m_Jump;
+	[SerializeField] private int m_PlayerId = 1;
+
+
+    private void Awake()
+    {
+		m_Character = GetComponent<PlayerMovement>();
+    }
+
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+			m_Jump = CrossPlatformInputManager.GetButtonDown("Vertical_P"+m_PlayerId);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+		float h = CrossPlatformInputManager.GetAxis("Horizontal_P"+m_PlayerId);
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, m_Jump);
+        m_Jump = false;
+    }
+}
