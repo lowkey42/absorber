c6f3252461f7753114728883e638cb75be519dfb
lowkey <mephistopheles@onlinehome.de>
2018-01-27 13:43:43 +0100

    basic win condition

---
+++ Assets/source/GameplaySystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplaySystem : MonoBehaviour {
+
+	private bool gameWon = false;
+
+	void LateUpdate () {
+		if (gameWon)
+			return;
+
+
+		var won = true;
+		foreach (TargetArea area in GameObject.FindObjectsOfType<TargetArea> ()) {
+			won = won && area.isActivated();
+		}
+
+		if (won) {
+			gameWon = true;
+			Time.timeScale = 0;
+			Debug.Log ("Level done!");
+			// TODO: show overlay, play sound, redirect to next level
+		}
+	}
+}
