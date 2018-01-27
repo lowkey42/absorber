62cc6b39070e080896916e697432c3150bb2c463
lowkey <mephistopheles@onlinehome.de>
2018-01-28 00:59:39 +0100

    title screen

---
+++ Assets/source/UIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	public Image titleImage;
+
+	public GameObject titleGo;
+
+	public bool showTitleScreen = true;
+
+	void Start() {
+		StartCoroutine (playTitleAnimation());
+	}
+
+	public IEnumerator playTitleAnimation() {
+		if (showTitleScreen) {
+			for (int i = 0; i < sprites.Length; i++) {
+				titleImage.sprite = sprites [i];
+				yield return new WaitForSeconds (2f / sprites.Length);
+			}
+
+			yield return new WaitForSeconds (1f);
+		}
+
+		titleGo.SetActive (false);
+	}
+
+}
